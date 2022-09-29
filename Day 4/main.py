0f2156e5ad8af7c129d6b304e87691f4e248b14d
Aditya Ervansyah <80872555+adenadit@users.noreply.github.com>
2022-09-29 23:36:21 +0700

    add days 4 and 5

---
+++ Day 4/main.py
@@ -0,0 +1,12 @@
+print("=== Your Adventure Simulator ===")
+print("""You'll be asked a bunch of questions
+then we'll make you up an amazing
+story with you as the star!""")
+print()
+name = input("Your name: ")
+enemy = input("Your worst enemy's name: ")
+superPower = input("Your super power: ")
+print()
+print(" Our story begins as hero name approaches a foreboding castle_")
+print("Suddenly a bolt of lighting striked the ground at the feet of", name)
+print("\033[31m'Our final battle begins'\033[0m shout the devil", enemy, "clearly missing the fact that", name, "has the power of\033[35m", superPower, "\033[0mwhich means thy'll win quite easily")
---
+++ Day 5/main.py
@@ -0,0 +1,18 @@
+
+choice1 = input("Do you like 'hanging around'?: ")
+if choice1 == "yes":
+  print("ohhhoooo... you're Spiderman")
+else:
+  print("then you're not Spiderman")
+  
+choice2 = input("Do you have a 'gravelly' voice: ")
+if choice2 == "yes":
+  print("i know, you're Korg")
+else:
+  print("aww, then you're not Korg")
+  
+choice3 = input("Do you often feel 'Marvelous'?: ")
+if choice3 == "yes":
+  print("Aha! you're Captain Marvel,hi!")
+else:
+  print("okay you're not Captain Marvel")
