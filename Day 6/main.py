d978025799d19f76d8f70c49e31744093547ab2a
Aditya Ervansyah <80872555+adenadit@users.noreply.github.com>
2022-09-30 16:33:52 +0700

    add files day 6

---
+++ Day 6/main.py
@@ -0,0 +1,14 @@
+print("MY LOGIN SYSTEM")
+print("+++++++++++++++")
+
+username = input("username: ")
+password = input("password: ")
+
+if username == "aditya" and password == "adit123":
+  print("hello", username)
+  print("welcome to the system")
+elif username == "adit" and password == "adit123":
+  print("hi", username)
+  print("can I help you??")
+else:
+  print("no you")
