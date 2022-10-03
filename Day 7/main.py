dad8a2d15448b0f97f093e8677973f7f20b39c7e
Aditya Ervansyah <80872555+adenadit@users.noreply.github.com>
2022-10-04 00:13:12 +0700

    add Day 7

---
+++ Day 7/main.py
@@ -0,0 +1,19 @@
+print("Fake Fan Finder")
+print("===============")
+anime = input("what's your favourite anime?? ")
+if anime == "One Piece":
+  char = input("oh really?? name me any of the character?")
+  if char == "Luffy":
+    job = input(" you got that by pure chance, okey then, what is him job on the ship")
+    if job == "Captain":
+      bounty = input("hmmmpp.. what was him first bounty then?")
+      if bounty == "30000000":
+        print("hello nakaaammaaaaa")  
+      else:
+        print("bye bye FAKEEE!!!")
+    else:
+      print("you FAKE")
+  else:
+    print("You fake")
+else:
+  print("okkee... you're not One Piece fans")
